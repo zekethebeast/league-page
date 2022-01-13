6e10c1dd64344d70517f9e28babaddb17c6a92c1
zekethebeast <97549096+zekethebeast@users.noreply.github.com>
2022-01-13 13:51:20 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -33,9 +33,9 @@ export const managers = [
   "favoriteTeam": "no", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
   "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
   "rival": {
-     name: "Rival", // Can be anything (usually your rival's name)
-      link: 6, // manager array number within this array, or null to link back to all managers page
-    image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+     name: "Myself", // Can be anything (usually your rival's name)
+      link: 1, // manager array number within this array, or null to link back to all managers page
+    image: "/managers/242924646_10208807188813154_1252338237121455447_n.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
     },
   "favoritePlayer": 289, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
   "valuePosition": "QB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
