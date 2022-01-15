f92d660173aba1b25332308e9bc0e8f0ee57ae78
zekethebeast <97549096+zekethebeast@users.noreply.github.com>
2022-01-15 16:57:07 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -114,7 +114,7 @@ export const managers = [
   "bio": "Father of 3, happily married, sports fan.",
   "photo": "/managers/everyone.jpg", // square ratio recommended (no larger than 500x500)
   "fantasyStart": 2010, // (optional) when did the manager start playing fantasy football
-  "favoriteTeam": "gb, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  "favoriteTeam": "gb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
   "mode": "Dynasty(optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
   "rival": {
         name: "Tyler Berry", // Can be anything (usually your rival's name)
