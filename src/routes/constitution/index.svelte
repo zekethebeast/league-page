240e548330f8e6bd91807ada301d32b9c876a732
zekethebeast <97549096+zekethebeast@users.noreply.github.com>
2022-02-21 15:25:41 -0600

    Update index.svelte

--- src/routes/constitution/index.svelte
+++ src/routes/constitution/index.svelte
@@ -123,14 +123,13 @@ u<script context="module">
     
         <h4 class="noUnderscore clickable" on:click={() => goToSection(fourOne)}>4.1 Overview</h4>
         <h4 class="noUnderscore clickable" on:click={() => goToSection(fourTwo)}>4.2 The Draft</h4>
-	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourThree)}>4.3 The Roster</h4>
-	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourFour)}>4.4 Free Agency</h4>
-	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourFive)}>4.5 Trades</h4>
-	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourFiveOne)}>4.5.1 Trade Veto</h4>
-	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourSix)}>4.6 Starting Lineups</h4>
-	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourSixOne)}>4.6.1 Position Designations</h4>
-	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourSeven)}>4.7 Active Ownership Requirements</h4>
-	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourEight)}>4.8 Anti‐Tanking Provision</h4>
+	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourThree)}>4.3 Free Agency</h4>
+	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourFour)}>4.4 Trades</h4>
+	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourFourOne)}>4.4.1 Trade Veto</h4>
+	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourFive)}>4.5 Starting Lineups</h4>
+	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourFiveOne)}>4.5.1 Position Designations</h4>
+	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourSix)}>4.6 Active Ownership Requirements</h4>
+	<h4 class="noUnderscore clickable" on:click={() => goToSection(fourSeven)}>4.7 Anti‐Tanking Provision</h4>
 
     <h3 class="noUnderscore clickable" on:click={() => goToSection(five)}>Section 5: Scoring System</h3>
     
@@ -170,7 +169,7 @@ Amendments will be brought up at each year’s draft and will be voted on by the
     <h3 bind:this={twoOne}>2.1 League Fees</h3>
     <p>The Armchair Football League fee for the 2021 season will be $75 from each owner. This fee consists of $50 entry fee plus $25 from each owner towards the 2022 season to ensure long term commitment. This fee is due on or before the draft. </p>
 
-    <p>Each subsequent year fee will be $52.50 (half of this years and half of next years) unless changed. $5 will go to the website hosting fee and the fee for the collection of dues. </p>
+    <p>Each subsequent year fee will be $52.50 (half of this years and half of next years) unless changed. $2.50 will go to the website hosting fee. </p>
 
     <h3 bind:this={twoTwo}>2.2 Prize Money</h3>
     <p>League prize money will be paid out within seven days of the completion of the Armchair Football League Super Bowl. Money will be all three held by the commissioner and will be paid out in the following manner: </p>
@@ -181,35 +180,96 @@ Amendments will be brought up at each year’s draft and will be voted on by the
   
     
   
-    <h2 class="sectionHeading" bind:this={three}>Section 3 Scoring System</h2>
+    <h2 class="sectionHeading" bind:this={three}>Section 3 League Set-up and Competition</h2>
 
-    <h3 bind:this={threeOne}>3.1 Scoring System</h3>
-    <p>The league scoring system breakdown is listed on the Sleeper fantasy app (for the most part, it follows typical .5PPR Scoring). The scoring system will never change without a majority league vote.</p>
-    
-    <h2 class="sectionHeading" bind:this={four}>Section 4 Postseason</h2>
-    
-    <h3 bind:this={fourOne}>4.1 Playoffs</h3>
-    <p>At the end of the season 6 teams will make the playoffs. The first round of playoffs will commence in week 15. Each team that wins their division and the 4 wild card teams that finish with the best records, will receive a playoff berth. The 2 teams that win their division will receive a bye week in the first round of the playoffs. Each playoff matchup will only last one week and the winner will advance. Week 18 will not be used.</p>
-    
-    <h3 bind:this={fourTwo}>4.2 Seeding Tiebreakers</h3>
-    <p>If two teams finish with the same record the tiebreakers will go as followed:</p>
+	<p>The Armchair Football League will consist of 10 different teams, broken into two conferences of 5 teams each. The schedule will be randomly generated, with each team playing every other team at least once in head‐to‐head match‐up. Teams will also play their division opponents an additional time for a 14‐ game regular season.   </p>
+	<p>In addition, each team will have a weekly game against the league median. Any team who is a top 5 scorer for the week will get a win, while those who don’t receive a loss.   </p>
 
-    <ol>
-        <li>Points For</li>
-        <li>Head 2 Head Record</li>
-        <li>Division Record</li>
-        <li>Total Points Against</li>
-        <li>Coin Flip</li>
-        <li>Duel to the death</li>
-    </ol>
 
-    <h2 class="sectionHeading" bind:this={five}>Section 5 Tanking Policy</h2>
+    <h3 bind:this={threeOne}>3.1 Division Rankings</h3>
+	<p>Teams will be ranked within each division based on the following criteria: </p>
+<p>Overall Winning Percentage </p>
+<p>Division Winning Percentage </p>
+<p>Head‐to‐head Winning Percentage </p>
+<p>Total Points Scored by the Starters ‐ Year to Date </p>
+<p>Total Points Scored by Head‐to‐head Opponents ‐ Year to Date
+</p>
+	
+	<h3 bind:this={threeTwo}>3.2 Playoffs</h3>
+    <p>After the completion of the regular season, three teams from each conference will make the playoffs. The top two teams from each division will make the playoffs, and the top two scorers league wide that already do not have a spot will be in the playoffs as wild cards. The championship will be in weeks 17 and 18. Matchups will be adjusted manually as needed. A consolation bracket will be used for everyone who does not make the playoffs.  </p> 
+<p>Playoffs will be reseeded each week. The Highest seed will always play the Lowest seed each round. Each division winner will get a bye in the first round of the playoffs.  </p>
+
+    <h2 class="sectionHeading" bind:this={four}>Section 4 Rosters and Lineups</h2>
+	
+	<p>Rosters in Armchair Football League consist of 29 active players from any NFL team. There are no position limits, and owners are free to have as many players from any position as they like. Only players from the active roster may be used as part of a team's starting lineup. </p>
+    
+    <h3 bind:this={fourOne}>4.1 The Draft</h3>
+    <p>The commissioner will designate a draft night each season. Owners are expected to attend the draft in person and must have all league fees paid before the draft. The draft order in year one will be randomly determined one week before the draft for the first part of the draft.   </p>
+<p>The initial draft will consist of three parts.   </p>
+<p>∙ 16 round Snake Order draft – Non‐Rookies only, order randomized before draft ∙ 5 round Linear Order draft – Rookies only, order will be reverse order of part 1 ∙ 8 round Linear Order Draft – Supplemental, any player not already taken, same order as round 1 </p>
+<p>After year one, the draft will consist of the following: </p>
+<p>∙ 3 round Linear Order draft – Rookies only, order based on reverse order of previous year standings </p>
+<p>∙ 5 round Linear Order Draft – Supplemental, order based on waiver priority at end of previous season </p>
+<p>Owners will have 2 minutes to make their picks. Any owner who does not make their pick in time will be skipped and will only have a 30 second window after the next team has made their selection. If they do not get it in time for the second time, their pick will automatically default to the end of the draft. 
+</p>
+    
+    <h3 bind:this={fourTwo}>4.2 The Roster</h3>
+	<p>Roster composition will consist of the following: </p>
+<p>∙ 10 Starters (detailed in section 4.5) 
+
+<p>∙ 16 Bench Players (No limits on positions) 
+
+<p>∙ 3 Injured Reserve Spots (This included players who miss because of COVID‐19) ∙ 3 Taxi Squad Spots </p>
+
+<p> Players with 3 years experience or less can be on a taxi squad </p>
+
+<p> Players must be placed on the Taxi Squad before the start of the regular season o Once a player is on a Taxi Squad, they will remain there until activated. Once the season begins, no player can be placed on the Taxi Squad.  </p>
+
+	   <h3 bind:this={fourThree}>4.3 Free Agency</h3>
+	<p>Free Agency will be Rolling Waiver setting. The order will be randomized after the initial draft. When a player is claimed off waivers, you will be placed at the bottom of the waiver priority. Your position is not lowered until you claim a player. Waivers will clear each Wednesday at 12:05am. A player is held on waivers 2 days after being dropped. A player who is a free agent should not reset your waiver claim status.   </p>
+	  
+	<h3 bind:this={fourFour}>4.4 Trades</h3>
+	<p>Trading is allowed and encouraged in Armchair Football League and may be conducted between any owner. Trades may include multiple players from any position, as well as draft picks. If an owner accepts a trade and that puts them over their roster limit, they must drop other players to stay under the limit. No trades will be allowed after Week 12 of the fantasy football season. </p>
+	
+	<h3 bind:this={fourFourOne}>4.4.1 Trade Veto</h3>
+	<p>The executive committee reserves the right to reverse any trade that they deem inconsistent with league competition and fair‐play standards. This veto will only be invoked in extreme cases where it is obvious that one owner is trying to give another owner an unfair advantage. The owners in question may offer reasons why the trade should be allowed, but the decision of the executive committee is final once it has been rendered. If the trade involves a committee member, they may offer reasons for the trade to be allowed but will not have a say in the final committee decision. </p>
+	
+	<h3 bind:this={fourFive}>4.5 Starting Lineups</h3>
+	<p>Starting lineups in Armchair Football League will consist as follows: </p>
+<p>1 quarterback </p>
+<p>2 running backs </p>
+<p>2 wide receivers </p>
+<p>1 tight end </p>
+<p>2 flex (running back, wide receiver, or tight end) </p>
+<p>1 Rec flex (wide receiver, or tight end) </p>
+<p>1 Superflex (quarterback, running back, wide receiver, or tight end) </p>
+<p>Players may be added or removed from the starting lineup up until the start of their game. Owners may make changes to their lineups but may not change a player that has already played. For example, if you started your quarterback on Thursday night and he has a bad game, you cannot switch him out for a quarterback who has not played. Also, if you did not start a wide receiver who played on Thursday night
+and he scored three touchdowns, you cannot add him to your lineup on Friday. Once the Thursday game begins, players from that game may not be started or benched. </p>
+
+	 
+	<h3 bind:this={fourFiveOne}>4.5.1 Position Designations</h3>
+	<p>From time to time, the NFL may change their designation of a particular player from one position to another or a player may line up in multiple positions throughout the game. For example, a wide receiver may be switched to a tight end and vice versa. For the purposes of Armchair Football League, a player’s designation will be at the discretion of the Sleeper App. </p>
+	
+	<h3 bind:this={fourSix}>4.6 Active Ownership</h3>
+	<p>Each owner is expected to actively manager their team each week, even if they are eliminated from any playoff consideration. If an owner does not submit a lineup for two consecutive weeks (determined by players on byes starting, injured players in a roster spot, etc.), the executive committee will have the right to assume control of the team and manage it for the rest of the season. A committee run team will not make any trades and is only allowed to add or drop players if the team cannot field a legal starting lineup due to injury or bye weeks. Any prize money won by a committee‐run team will be distributed evenly to the other active owners in the league.   </p>
+	
+	<h3 bind:this={fourSeven}>4.2 Anti Tanking Provision</h3>
+	<p>Armchair Football League is a competitive league, created for the league owners to have fun playing fantasy football. While the competition may be fierce, this is not considered a cut‐throat league. In the spirit of competition and league fairness, owners are not allowed to intentionally create a legal starting lineup that gives the opposing team a clear advantage. The executive committee reserves the right to step in and adjust the starting lineup of a team that is clearly trying to intentionally lose a game. If the committee must adjust a team's lineup in two consecutive weeks or for three or more weeks in any one season, the team will be considered inactive and the executive committee may assume ownership of the team as detailed in the active ownership requirements section. </p>
+	
+
+
+    <h2 class="sectionHeading" bind:this={five}>Section 5 Scoring System</h2>
+	<p>Scoring in Armchair Football League will be computed to two decimal places. This will allow points to be awarded or deducted for every positive or negative yard and will dramatically reduce the chance of a tie game. Players are awarded fantasy points for each week that they are included in the team's starting lineup.   </p>
+	<p> The scoring breakdown can be seen on the Sleeper App and Website.</p>
     
   
-    <h2 class="sectionHeading" bind:this={six}>Section 6 Replacing Managers</h2>
-    
+    <h2 class="sectionHeading" bind:this={six}>Section 6 Offseason Activities</h2>
+    <p>Rosters will be locked at the end of each season. Trades may still occur leading up to the Draft, but no free agents will be allowed to be added until after the Supplemental Draft is finished.  </p>
    
-    <h2 class="sectionHeading" bind:this={seven}>Section 7 League Finances</h2>
+    <h2 class="sectionHeading" bind:this={seven}>Section 7 Change Log</h2>
+	<p>08/18/2021 - Switched the number of rounds in each year’s draft to 3 Rookie Only (from 5) and 5 Supplemental (from 3) to align with limitations on pick trading within Sleeper. </p>
+	<p>01/11/2022 - Changed fee to $52.50 per year to cover maintenance costs. </p>
+
     
   
 
